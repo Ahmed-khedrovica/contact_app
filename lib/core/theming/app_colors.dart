11dfb24757f5f9a3d479218f2ce386d66931b282
Your Name <you@example.com>
2025-12-18 04:33:17 +0200

    - Added Animation files.

---
+++ lib/core/theming/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+abstract final class AppColors {
+  static final mainDarkBlue = Color(0xff29384D);
+  static final lighterBlue = Color(0xffE2F4F6);
+  static final offWhite = Color(0xffFFF1D4);
+  static final white = Color(0xffffffff);
+
+}
