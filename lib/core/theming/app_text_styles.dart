46361c3ea33be21a91ba34fb6d5214bc6c3eacc2
Your Name <you@example.com>
2025-12-18 03:50:22 +0200

    - Added app_text_styles class.

---
+++ lib/core/theming/app_text_styles.dart
@@ -0,0 +1,10 @@
+import 'package:contact/core/theming/app_colors.dart';
+import 'package:flutter/material.dart';
+
+abstract final class AppTextStyles {
+  static final font20OffWhiteMedium = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: AppColors.offWhite,
+  );
+}
