37e0033d4cd491dbec2c74d592737a1532637c6f
Your Name <you@example.com>
2025-12-20 02:56:58 +0200

    - Added bottom sheet elevated button.

---
+++ lib/features/home/ui/widgets/bottom_sheet_elevated_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/theming/app_colors.dart';
+import '../../../../core/theming/app_text_styles.dart';
+
+class BottomSheetElevatedButton extends StatelessWidget {
+  const BottomSheetElevatedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.offWhite,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        onPressed: () {
+          // logic
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Text(
+            'Enter user',
+            style: AppTextStyles.font20DarkBlueRegular,
+          ),
+        ),
+      ),
+    );
+  }
+}
